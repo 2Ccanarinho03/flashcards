dd20191453a1fc2a8ed7864b125bdaf2ddaab954
2Ccanarinho03 <00001116672868sp@al.educacao.sp.gov.br>
2024-12-02 09:02:40 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'História',
+    'Em que ano ocorreu a queda do Muro de Berlim?',
+    'Em 1989'
+)
+
+criaCartao(
+    'Conhecimentos Gerais',
+    'Quem pintou a "Última Ceia"?',
+    'Leonardo da Vinci'
+)
+
+criaCartao(
+    'Matemática',
+    'Qual é o valor de 45 + 78?',
+    '123'
+)
+
+criaCartao(
+    'Química',
+    'Qual é o gás mais abundante na atmosfera terrestre?',
+    'Nitrogênio - 78%'
+)
